49de27888ee0d4e1e7a6bce9454d696131271f7c
Yo-wei Teng <ranyouwei3@gmail.com>
2014-05-29 09:24:56 +0800

    Update router.js

--- node-example/HelloApp/router.js
+++ node-example/HelloApp/router.js
@@ -1,6 +1,16 @@
 function route(pathname){
     console.log('request',pathname);
-    
+    // 檢查 pathname 是否有對應的 request handlers 
+    if (typeof handlers[pathname] == "function") {
+        
+        console.log('hi',pathname);
+    }else{
+        console.log('No request handler for this pat\
+hname:',pathname);
+    }
 }
 
 exports.route = route;
+
+
+
