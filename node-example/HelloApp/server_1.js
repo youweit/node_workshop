0d423be82a48ef6e1b60e15a72958e37f038135e
Yo-wei Teng <ranyouwei3@gmail.com>
2014-05-29 09:22:54 +0800

    Update server_1.js

--- node-example/HelloApp/server_1.js
+++ node-example/HelloApp/server_1.js
@@ -1,4 +1,12 @@
 var hello = require('./HelloModule_1.js');
 var router = require('./router.js');
+var handlers = require("./requestHandlers");
 
-hello.start(router.route);
+// 使用 Object 來對應 pathname 與 request handlers 
+var req = {
+    "/": handlers.index,
+    "/hi": handlers.index,
+}
+
+// 傳遞 request handler
+server.start(router.route, req);
